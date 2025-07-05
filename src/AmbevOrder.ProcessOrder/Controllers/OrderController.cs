d6e795729b73e8c25d617114814ddc16745bf80f
RD-Ricardo <ricardodias169@gmail.com>
2025-07-04 22:11:29 -0300

    fix: route endpoint

--- src/AmbevOrder.ProcessOrder/Controllers/OrderController.cs
+++ src/AmbevOrder.ProcessOrder/Controllers/OrderController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace AmbevOrder.ProcessOrder.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class OrderController : ControllerBase
     {
         [HttpGet]
